d6d729654d91afedd2a0c7f7e678a5482a5e3d23
rizalkalam <rizalkalam78@gmail.com>
2024-12-13 01:58:21 +0700

    fix configure server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000; // Menentukan port aplikasi (pastikan POR
 connectDB();
 
 // Jalankan server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
