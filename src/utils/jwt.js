ec214cb3a2b077355313bbedcb2d44539af329a1
rizalkalam <rizalkalam78@gmail.com>
2024-12-25 17:15:53 +0700

    fix feat register and resendverifycode

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,28 +1,8 @@
 // utils/jwt.js
 const jwt = require('jsonwebtoken');
-const redisClient = require('../config/redisClient')
 
 const generateToken = (userId) => {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET_KEY || 'mysecretkey12345!@#security', { expiresIn: '1h' });
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET_KEY || 'mysecretkey12345!@#security', { expiresIn: '7d' });
 };
 
-const generateRefreshToken = async (userId) => {
-    const refreshToken = jwt.sign({ id: userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
-
-    // Simpan refresh token ke Redis dengan waktu kedaluwarsa (7 hari) menggunakan 'EX' untuk expiration time
-    await redisClient.set(`refreshToken:${refreshToken}`, userId);
-    await redisClient.expire(`refreshToken:${refreshToken}`, 7 * 24 * 60 * 60); // Set EX manually (7 days in seconds)
-
-    return refreshToken;
-};
-
-const validateRefreshToken = async (refreshToken) => {
-    const userId = await redisClient.get(`refreshToken:${refreshToken}`);
-    return userId;
-};
-
-const revokeRefreshToken = async (refreshToken) => {
-    await redisClient.del(`refreshToken:${refreshToken}`);
-};
-
-module.exports = { generateToken, generateRefreshToken, validateRefreshToken, revokeRefreshToken };
+module.exports = { generateToken};
