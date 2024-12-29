57b60d34b2c9a8f699c9aa9b0fd8b270462b9ad4
rizalkalam <rizalkalam78@gmail.com>
2024-12-29 16:40:36 +0700

    fix auth register

--- src/routes/forgotPasswordRoutes.js
+++ src/routes/forgotPasswordRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { verifyEmail, verifyCode, newPassword, sendConfirmationEmail } = require('../controllers/forgotPasswordController');
+const { verifyEmail, resendVerifyEmail, verifyCode, newPassword, sendConfirmationEmail } = require('../controllers/forgotPasswordController');
 const router = express.Router();
 
 /**
@@ -28,6 +28,57 @@ const router = express.Router();
  *         description: Permintaan tidak valid
  */
 router.post('/verify_email', verifyEmail);
+/**
+ * @swagger
+ * /resend_verify_email:
+ *   post:
+ *     summary: Resend verification email
+ *     description: Mengirimkan kode verifikasi baru ke email yang diberikan.
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email pengguna yang akan dikirimkan kode verifikasi.
+ *                 example: "user@example.com"
+ *     responses:
+ *       200:
+ *         description: Kode verifikasi baru berhasil dikirim.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "New verification code sent to your email. Please check your inbox."
+ *       400:
+ *         description: Terjadi kesalahan saat mengirimkan kode verifikasi.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Error resending verification email"
+ *       404:
+ *         description: Email tidak ditemukan atau tidak ada kode verifikasi terkait.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Verification code not found for the provided email"
+ */
+router.post('/resend_verify_email', resendVerifyEmail);
 /**
  * @swagger
  * /forgot_password/verify_code:
@@ -73,7 +124,7 @@ router.post('/verify_code', verifyCode);
  * @swagger
  * /forgot_password/new_password:
  *   post:
- *     summary: Buat Password Baru
+ *     summary: Update password pengguna setelah verifikasi kode
  *     tags: [Forgot Password]
  *     requestBody:
  *       required: true
@@ -82,15 +133,22 @@ router.post('/verify_code', verifyCode);
  *           schema:
  *             type: object
  *             properties:
+ *               verificationCode:
+ *                 type: string
+ *                 description: Kode verifikasi yang diterima pengguna untuk mereset password
+ *                 example: "1234"
  *               newPassword:
  *                 type: string
  *                 format: password
  *                 description: Password baru pengguna
+ *                 example: "newpassword123"
  *               confirmPassword:
  *                 type: string
  *                 format: password
  *                 description: Konfirmasi password baru
+ *                 example: "newpassword123"
  *             required:
+ *               - verificationCode
  *               - newPassword
  *               - confirmPassword
  *     responses:
@@ -103,7 +161,7 @@ router.post('/verify_code', verifyCode);
  *               properties:
  *                 message:
  *                   type: string
- *                   example: Password updated successfully
+ *                   example: "Password updated successfully"
  *       400:
  *         description: Permintaan tidak valid (password tidak cocok atau kode verifikasi diperlukan)
  *         content:
@@ -113,7 +171,7 @@ router.post('/verify_code', verifyCode);
  *               properties:
  *                 message:
  *                   type: string
- *                   example: Password and confirm password do not match
+ *                   example: "Password and confirm password do not match"
  *       404:
  *         description: Pengguna tidak ditemukan
  *         content:
@@ -123,9 +181,17 @@ router.post('/verify_code', verifyCode);
  *               properties:
  *                 message:
  *                   type: string
- *                   example: User not found
+ *                   example: "User not found"
  *       500:
  *         description: Kesalahan server
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Server error."
  */
 router.post('/new_password', newPassword);
 // ---
