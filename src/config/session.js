6c383791661917789060055fdecb53b463b3122c
rizalkalam <rizalkalam78@gmail.com>
2025-04-03 01:29:12 +0700

    fix cookie config

--- src/config/session.js
+++ src/config/session.js
@@ -10,7 +10,7 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: false,
   cookie: { 
-        secure: false,
+        secure: true,
         sameSite: 'None',
         httpOnly: true,
         maxAge: 5 * 60 * 1000,  // session expires after 5 minutes
